6459bdcb1d3b6e3476fe2025a28588a3956f2e43
kbarr1 <kbarr@kth.se>
2016-09-08 11:35:37 +0200

    Lagt till en loop i start.html som hämtar data från view.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största 1slagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
+
 # Create your views here.
 
 def start(request):
-	return render(request, "quiz/start.html")
+	context = {
+	"quizzes": quizzes,
+	}
+	return render(request, "quiz/start.html", context)
 
 def quiz(request):
+
 	return render(request, "quiz/quiz.html")
 
 def question(request):
