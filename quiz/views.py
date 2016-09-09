9f44092dfbfb48420e4d0b6a60855d7503153642
kbarr1 <kbarr@kth.se>
2016-09-09 11:51:10 +0200

    La till så man räknar ut korrekta svar i results.py

--- quiz/views.py
+++ quiz/views.py
@@ -56,10 +56,21 @@ def answer(request, quiz_number, question_number):
 
     
 def results(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	saved_answers = request.session.get(quiz_number, {})
+
+	num_correct_answers = 0
+
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+		if correct_answer == answer:
+			num_correct_answers += 1
+	
 	context = {
 	"quizzes": Quiz.objects.all(),
-	"correct": 12,
-	"total": 20, 
+	"correct": num_correct_answers,
+	"total": questions.count(), 
 	"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/results.html", context)
